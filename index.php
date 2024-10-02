fa077e29be745b4f36fe620884522af89e11b97f
Felipe Naves <62889875+frnaves@users.noreply.github.com>
2024-10-02 20:28:00 -0300

    codigos e pastas
    novos codigos

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Tarefas com PHP</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu:wght?@400;500;700&display=swap">
+    
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Gerenciador de Tarefas</h1>
+
+
+        </div>
+
+        <div class="form">
+            <form action="" method="get">
+                <label for="task_name">Tarefa</label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa">
+                <button type="submit">Cadastrar</button>
+
+            </form>
+
+
+
+        </div>
+
+        <div class="separator">
+
+        </div>
+
+        <div class="list-tasks">
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+
+        </div>
+
+        <div class="footer">
+            <p>Desenvolvido por FRNaves através do curso <a href="#"></a>Link do Curso</p>
+
+        </div>
+    </div>
+
+    
+</body>
+</html>
